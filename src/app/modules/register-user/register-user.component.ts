72acb715ad2e39cdaa74cc6a3da6a28e5a4cd5a6
Ioannis Daniil <jondan97@gmail.com>
2022-09-26 18:28:06 +0300

    created form structure (will be turned to reactive form later)

--- src/app/modules/register-user/register-user.component.ts
+++ src/app/modules/register-user/register-user.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterUserComponent implements OnInit {
 
+  regions = ["Choose a region...*", "Attica", "Central Greece", "Central Macedonia", "Crete",
+    "Eastern Macedonia and Thrace", "Epirus", "Ionian Islands", "North Aegean",
+    "Peloponnese", "South Aegean", "Thessaly", "Western Greece", "Western Macedonia", "Mount Athos"];
+
+
   constructor() { }
 
   ngOnInit(): void {
