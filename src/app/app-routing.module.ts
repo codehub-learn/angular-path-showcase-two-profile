347393e621e1e2ff207527a97ec89b659c9712f3
Ioannis Daniil <jondan97@gmail.com>
2022-09-26 17:31:14 +0300

    finalized with application's structure and template

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./modules/home/home.component";
+import {ViewAllUsersComponent} from "./modules/view-all-users/view-all-users.component";
+import {RegisterUserComponent} from "./modules/register-user/register-user.component";
+import {NotFoundError} from "rxjs";
+import {NotFoundErrorComponent} from "./core/not-found-error/not-found-error.component";
+
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  },
+  {
+    path: "users",
+    component: ViewAllUsersComponent
+  },
+  {
+    path: "registerUser",
+    component: RegisterUserComponent
+  },
+  {
+    path: "**",
+    component: NotFoundErrorComponent
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./core/header/header.component";
+import {FooterComponent} from "./core/footer/footer.component";
+import {RouterModule} from "@angular/router";
+import { HomeComponent } from './modules/home/home.component';
+import { ViewAllUsersComponent } from './modules/view-all-users/view-all-users.component';
+import { RegisterUserComponent } from './modules/register-user/register-user.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { NotFoundErrorComponent } from './core/not-found-error/not-found-error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ViewAllUsersComponent,
+    RegisterUserComponent,
+    NotFoundErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
